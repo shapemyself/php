bd0b03cac7fd23b2866ac5e38010ca4eae8e274b
刘恒胜 <liuhengsheng@xiaomi.com>
2017-03-04 23:12:38 +0800

    redis use just test

--- ownphplib/RedisUse.php
+++ ownphplib/RedisUse.php
@@ -152,11 +152,139 @@ function samplePulishAndSub() {
 }
 
 
+//简单极速器的使用
+function sampleCounter() {
+    $redis = getConnection();
+    $strKey = 'Test_bihu_comments';
+
+//设置初始值
+    $redis->set($strKey, 0);
+
+    $redis->INCR($strKey);  //+1
+    $redis->INCR($strKey);  //+1
+    $redis->INCR($strKey);  //+1
+
+    $strNowCount = $redis->get($strKey);
+
+    echo "---- 当前数量为{$strNowCount}。 ---- ";
+}
+
+//简单计数器的使用
+function sampleSort() {
+    $redis = getConnection();
+
+    $strKey = 'Test_bihu_score';
+
+//存储数据
+    $redis->zadd($strKey, '50', json_encode(['name' => 'Tom']));
+    $redis->zadd($strKey, '70', json_encode(['name' => 'John']));
+    $redis->zadd($strKey, '90', json_encode(['name' => 'Jerry']));
+    $redis->zadd($strKey, '30', json_encode(['name' => 'Job']));
+    $redis->zadd($strKey, '100', json_encode(['name' => 'LiMing']));
+
+    $dataOne = $redis->ZREVRANGE($strKey, 0, -1, true);
+    echo "---- {$strKey}由大到小的排序 ---- <br /><br />";
+    print_r($dataOne);
+
+    $dataTwo = $redis->ZRANGE($strKey, 0, -1, true);
+    echo "<br /><br />---- {$strKey}由小到大的排序 ---- <br /><br />";
+    print_r($dataTwo);
+}
 
 
 
+//简单字符串悲观锁
 
-samplePulishAndSub();
+
+function lock($key = '', $expire = 5) {
+    $redis = getConnection();
+    //setnax(key,value):  将 key 的值设为 value ，当且仅当 key 不存在。
+    $is_lock = $redis->setnx($key, time()+$expire);
+    //不能获取锁
+    if(!$is_lock){
+        //判断锁是否过期
+        $lock_time = $redis->get($key);
+        //锁已过期，删除锁，重新获取
+        if (time() > $lock_time) {
+            unlock($key);
+            $is_lock = $redis->setnx($key, time() + $expire);
+        }
+    }
+
+    return $is_lock? true : false;
+}
+
+/**
+ * 释放锁
+ * @param  String  $key 锁标识
+ * @return Boolean
+ */
+function unlock($key = ''){
+    $redis = getConnection();
+    return $redis->del($key);
+}
+
+
+function sampleUnhappyLock() {
+    // 定义锁标识
+    $key = 'Test_bihu_lock';
+
+    // 获取锁
+    $is_lock = lock($key, 10);
+    if ($is_lock) {
+        echo 'get lock success<br>';
+        echo 'do sth..<br>';
+        sleep(5);
+        echo 'success<br>';
+        unlock($key);
+    } else { //获取锁失败
+        echo 'request too frequently<br>';
+    }
+
+}
+
+
+/**
+ * @bref:乐观锁的实现，乐观锁的意思是：每次修改前都假设不会发生修改
+ */
+function sampleHappyKey() {
+
+    $redis = getConnection();
+
+    $strKey = 'Test_bihu_age';
+
+    $redis->set($strKey,10);
+
+    $age = $redis->get($strKey);
+
+    echo "---- Current Age:{$age} ---- <br/><br/>";
+
+    $redis->watch($strKey,10);
+
+// 开启事务
+    $redis->multi();
+
+//在这个时候新开了一个新会话执行
+    $redis->set($strKey,30);  //新会话
+
+    echo "---- Current Age:{$age} ---- <br/><br/>"; //30
+
+    $redis->set($strKey,20);
+
+    $redis->exec();
+
+    $age = $redis->get($strKey);
+
+    echo "---- Current Age:{$age} ---- <br/><br/>"; //30
+
+//当exec时候如果监视的key从调用watch后发生过变化，则整个事务会失败
+
+}
+
+sampleHappyKey();
+
+
+
 
 
 
